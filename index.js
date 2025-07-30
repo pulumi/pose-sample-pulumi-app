2a648611786e187e1cd23060a4c03638bfc560f5
Alberto Pose <albertopose@gmail.com>
2025-07-30 15:14:39 +0100

    Changing NodeJS runtime to 18.

--- index.js
+++ index.js
@@ -4,15 +4,30 @@
 // TODO: Oops I got the wrong one. I need to use @pulumi/aws-apigateway.
 // I'm really confused: what's the difference between aws and aws-apigateway?
 const pulumi = require("@pulumi/pulumi");
+// Subset of `awsx` (v1). Another component provider. Wraps `aws`.
+// High-level construct.
+// L2
+// SUGGESTION: awsx.apigateway? So it reduces the confusion
 const apigateway = require("@pulumi/aws-apigateway");
+// Primitives. Expose primitives. REST-ful provider version. Generated.
+// API in provider form. Creates resources in AWS.
+// L1
+// Version:
 const aws = require("@pulumi/aws");
+// Crosswalk / higher level abstactions/wrapper around some parts of AWS
+// best practices, etc. Hand-written.
+// L2
+// Version:
 const awsx = require("@pulumi/awsx");
+const { Runtime } = require("@pulumi/aws/lambda");
+awsx.apigateway
 // TODO: Where is this?
 // const apigateway = require("@pulumi/aws-apigateway");
 
 
 // A Lambda function to invoke.
 const eventHandler = new aws.lambda.CallbackFunction("handler", {
+    runtime: Runtime.NodeJS18dX,
     callback: async (event, context) => {
         return {
             statusCode: 200,
