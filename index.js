eb7e07ccc7b7d817ff99d9344c4b4fde1c2eaa6d
Alberto Pose <albertopose@gmail.com>
2025-07-30 11:16:01 +0100

    First commit.

---
+++ index.js
@@ -0,0 +1,66 @@
+// TODO This link is broken.
+// Import the [pulumi/aws](https://pulumi.io/reference/pkg/nodejs/@pulumi/aws/index.html) package
+// TODO: Ah I was using TS instead of JS.
+// TODO: Oops I got the wrong one. I need to use @pulumi/aws-apigateway.
+// I'm really confused: what's the difference between aws and aws-apigateway?
+const pulumi = require("@pulumi/pulumi");
+const apigateway = require("@pulumi/aws-apigateway");
+const aws = require("@pulumi/aws");
+const awsx = require("@pulumi/awsx");
+// TODO: Where is this?
+// const apigateway = require("@pulumi/aws-apigateway");
+
+
+// A Lambda function to invoke.
+const eventHandler = new aws.lambda.CallbackFunction("handler", {
+    callback: async (event, context) => {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Hello from API Gateway!",
+            }),
+        };
+    },
+});
+
+// Create a public HTTP endpoint (using AWS APIGateway)
+// TODO This was awsx on the example, but it's not working.
+// const endpoint = new aws.apigateway.API("hello", {
+//     routes: [
+//         // Serve static files from the `www` folder (using AWS S3)
+//         {
+//             path: "/",
+//             localPath: "www",
+//         },
+
+//         // Serve a simple REST API on `GET /name` (using AWS Lambda)
+//         {
+//             path: "/source",
+//             method: "GET",
+//             eventHandler,
+//         },
+//     ],
+// });
+// Ahhh I was using the wrong one. It's not. It's RESTAPI.
+
+// A REST API to route requests to the Lambda function.
+// TODO Wrong: it's RestApi, not RestAPI.
+// https://www.pulumi.com/docs/iac/clouds/aws/guides/api-gateway/
+// const endpoint = new aws.apigateway.RestApi("api", {
+const endpoint = new apigateway.RestAPI("api", {
+    routes: [
+        {
+            path: "/source",
+            method: "GET",
+            eventHandler,
+        },
+        {
+            path: "/",
+            localPath: "www",
+        },
+    ],
+});
+
+
+// Export the public URL for the HTTP service
+exports.url = endpoint.url;
